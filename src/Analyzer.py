4d4b4de1181ffebc744a05815432c1804a459120
mlbrown08 <mlbrown7@uncg.edu>
2014-03-29 12:24:02 -0400

    Updated the Analyer to handle syntax errors in students code

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,109 +1,150 @@
 import sys
 import io
 import difflib
+from tkinter import *
 
 class Analyzer:
-    # represents the knowledge base, note - students code would not 
-    # be in knowledge base but for simplicity put it here
-    def knowledge_base(self):
-        # problem is the problem for the student to solve
-        self.problem = "Perfom three attacks using attack() function in a loop and then block using the block() function."
-        
-        # template is used to turn students code to print statements
-        self.template = "def attack():\n\tprint(\"You Attacked\")\ndef block():\n\tprint(\"You Blocked\")\n"
-        
-        # correct_output is the output that the student should get
-        # note - has to have a \n character on end to get correct output
-        self.correct_output = "You Attacked\nYou Attacked\nYou Attacked\nYou Blocked\n"
-        
-        # optimal_keywords are the word that student's solution should contain
-        self.optimal_keywords = ['for', 'attack()', 'block()']
-        
-        # students_code represents the students solution to the problem
-        self.students_code = "for x in range(3):\n\tattack()\nblock()"
-        
-    # stub method for the problem
-    def show_problem(self):
-        print("The problem is:")
-        print(self.problem)
-        print()
-        print("The students code is:")
-        print(self.students_code)
 
     # executes the student code and captures the output
-    def execute_code(self):
-        # code is the the students code and the template concatenated together
-        self.code = self.template + self.students_code
-        print()
-        print('Executed code below')
-        print(self.code)
+    def execute_code(self, students_code):
+        # Reads in the game's defintions used by the student
+        definitions = open('Student_Definitions.py', 'r')
+        definitions = definitions.read()
+
+        # Concatenates the defintions and students code together
+        self.code = definitions + students_code
 
         # students_output is the captured output from the students code
         self.students_output = io.StringIO()
         sys.stdout = self.students_output
         
-        # executes the student's code
-        exec(self.code)
-
-        # returns stdout to original stdout
-        sys.stdout = sys.__stdout__
-        
-        print()
-        print('Output is...')
-        print (self.students_output.getvalue())
-
+        try: 
+            # executes the student's code
+            exec(self.code)
+        except:
+            # restore stdout and stderr
+            sys.stdout = sys.__stdout__
+            self.syntax_error_hint()            
+            return True
+        else:
+            # returns stdout to original stdout
+            sys.stdout = sys.__stdout__
+            return False
+    
     # checks to see if student's output is correct
-    def check_output(self):
-        print()
-        print("The correct output is:")
-        print(self.correct_output)
-        print()
-        
+    def check_output(self, correct_output):
         # returns true or false if the student has correct output
-        if self.students_output.getvalue() == self.correct_output:
+        if self.students_output.getvalue() == correct_output:
             return True
         else:
             return False
 
     # checks to see if the students solution is correct
-    def check_students_solution(self):
+    def check_students_solution(self, students_code, optimal_solution, hints):
         # can use to get ratio of matched vs unmatched
         # self.seq = difflib.SequenceMatcher(None, self.students_code.lower(), self.optimal_solution.lower())
         
         # checks to see if the student's solution contain all the necessary
-        # keywords and if one is missing saves it in the variable error
-        for keyword in self.optimal_keywords:
-            if keyword in self.students_code:
+        # keywords and if false determines what error they had
+        i = 0
+        for keyword in optimal_solution:
+            if keyword in students_code:
                 self.analysis = True
+                i = i + 1 
             else:
                 self.analysis = False
-                self.error = keyword
+                self.hint = hints[i]
                 break
 
-    # analyzes student solutions - will check both output and solution and
+    # This method will execute and analyze the students code. 
+    # Only need to call this method from UI
+    # Analyzes student solutions - will check both output and solution and
     # decides if the student's solution is correct
-    def analyze_code(self):
-        self.output = self.check_output()
-        self.check_students_solution()
+    # Will return true if the solution is correct
+    # syntax_error is a boolean, which will be true if the is a syntax error
+    # result is a string that contains the message/hint for the student
+    def analyze_code(self, students_code, correct_output, optimal_solution, hints):
         
-        if self.output == True and self.analysis == True:
-            print("You got the solution correct")
+        self.syntax_error = self.execute_code(students_code)
+        if self.syntax_error == False:
+            self.output = self.check_output(correct_output)
+            self.check_students_solution(students_code, optimal_solution, hints)
+        
+        if self.syntax_error == True:
+            self.result = self.hint
+            return False
+        elif self.output == True and self.analysis == True:
+            self.result = "Great Job. You got the solution correct."
+            return True
         elif self.output == False and self.analysis == True:
-            print("You did not get the correct solution")
-            print("Your output is not correct")
+            self.result = '''You did not get the correct solution. Your output is not correct.''' 
+            return False
         elif self.output == True and self.analysis == False:
-            print("The output is correct but the code in not the most efficent")
-            print("Why don't you try again using {} statement".format(self.error))
+            self.result = '''The output is correct but the code in not the most efficent. Why don't you try again using a ''' + self.hint + '.'
+            return False
         else:
-            print("Your output is not correct and the code is not the most efficent")
-            print("Why don't you try again using {} statement".format(self.error))
+            self.result = '''Your solution in not correct. Why don't you try again using ''' + self.hint + '.'
+            return False
 
-                          
-def run():
+    # If there is a syntax error while capture it and 
+    # put it in a much more readable format
+    def syntax_error_hint(self):
+        # Type of Error
+        e = sys.exc_info()[0]
+        
+        # Description of the error
+        m = sys.exc_info()[1]
+        
+        # Formats the type of error 
+        error = str(e)
+        error = error[8:]
+        error = error[:len(error) - 2]
+        
+        # Formates description of the error
+        desc = str(m)
+        # gets line number of error if there is one
+        index = desc.find('line')
+        if index > 0:
+            line = desc[index:]
+            line = line[:6]
+            line = 'on ' + line
+        else:
+            line = ''
+        # removes '()' around description 
+        index = desc.find('(')
+        if index > 0:
+            desc = desc[:index]
+        
+        self.hint = 'You had an error in your code. The error is a ' + error + '. This means that there is ' + desc + line + '.'
+
+# Stub for testing - run test to test it
+def test():
+    print('Problem - Write a piece of code that prints the numbers 0 - 4\n')
+    students_code = '''x = 0
+while(x <= 5):
+    attack()
+    x = x + 1'''
+    print('Student\'s Code:')
+    print(students_code)
+    print()
+    correct_output = '''0
+1
+2
+3
+4
+'''
+    
+    optimal_solution = 'attack()', 'while'
+    hints = 'an attack() statement', 'a for loop'
     analyzer = Analyzer()
-    analyzer.knowledge_base()
-    analyzer.show_problem()
-    analyzer.execute_code()
-    analyzer.analyze_code()
+    result = analyzer.analyze_code(students_code, correct_output, optimal_solution, hints)
+    output = analyzer.students_output.getvalue()
+    print('Student\'s Output: \n' + output + '\n')
+    '''
+    if analyzer.syntax_error == True:
+        print('\nType of error is syntax error\n')
+    else:
+        print('\nType of error is not optimal soln\n')
+'''
+    print(analyzer.result)
 
-run()
